5ffdb86759b30b2a05bafcdc20d9e9f9e205a9c0
Eugene Whatley <whatleytechnologies@gmail.com>
2024-10-19 20:21:08 -0400

    provide option for output

--- src/create-prompt.mjs
+++ src/create-prompt.mjs
@@ -9,6 +9,16 @@ const promptAI = async (input) => {
   const personality = input?.personality ?? "You are a helpful assistant.";
   const aiPrompt =
     input?.aiPrompt ?? "Describe what it means to live a healthy lifestyle.";
+  const outputType = input?.outputType ?? "HTML";
+
+  let outputTypeDescription = "";
+  if (outputType === "HTML") {
+    outputTypeDescription =
+      "And respond only with HTML page for the result without the code block markdown.";
+  } else if (outputType === "Markdown") {
+    outputTypeDescription =
+      "And respond only with the markdown for the result without the code block markdown..";
+  }
 
   const result = await openai.chat.completions.create({
     model: "gpt-4o-mini",
@@ -16,17 +26,14 @@ const promptAI = async (input) => {
       { role: "system", content: personality },
       {
         role: "user",
-        content: aiPrompt,
+        content: `${aiPrompt}. ${outputTypeDescription}`,
       },
     ],
   });
 
   return {
     output: result,
-    input: {
-      personality,
-      aiPrompt,
-    },
+    input,
   };
 };
 
@@ -43,7 +50,7 @@ const persistUsage = (result) => {
     updated_at: date.format(),
   };
 
-  const fileName = `usage-${records.length + 1}-${date.format(
+  const fileName = `record-${records.length + 1}-${date.format(
     "YYYYMMDDHHmmssSSS"
   )}.json`;
   fs.writeFileSync(`db/${fileName}`, JSON.stringify(record));
@@ -51,7 +58,28 @@ const persistUsage = (result) => {
   return result;
 };
 
+const createWebpage = (result) => {
+  fs.mkdirSync("output", { recursive: true });
+
+  const date = dayjs();
+  const records = fs.readdirSync("output");
+  const content = result.output.choices[0].message.content;
+  const fileExt = result.input.outputType === "HTML" ? "html" : "md";
+
+  const fileName = `result-${records.length + 1}-${date.format(
+    "YYYYMMDDHHmmssSSS"
+  )}.${fileExt}`;
+
+  fs.writeFileSync(`output/${fileName}`, content);
+
+  return fileName;
+};
+
 export const execute = async (input) =>
   await promptAI(input)
     .then((result) => persistUsage(result))
-    .then((result) => result.output.choices[0].message.content);
+    .then((result) =>
+      result.input.outputType === "Console"
+        ? result.output.choices[0].message.content
+        : createWebpage(result)
+    );
--- src/index.mjs
+++ src/index.mjs
@@ -1,26 +1,39 @@
 import inquirer from 'inquirer';
 import { execute } from './create-prompt.mjs';
+import exec from "child_process";
 
 const questions = [
   {
-    type: 'input',
-    name: 'personality',
+    type: "input",
+    name: "personality",
     message: "What personality do you want the AI to have?",
   },
   {
-    type: 'input',
-    name: 'aiPrompt',
+    type: "input",
+    name: "aiPrompt",
     message: "What prompt do you want to give the AI?",
-  }
+  },
+  {
+    type: "list",
+    message: "What type of output do you want?",
+    name: "outputType",
+    choices: ["HTML", "Markdown", "Console"],
+  },
 ];
 
 const promptResults = await inquirer.prompt(questions);
 
 
 const input = {
-    personality: promptResults.personality,
-    aiPrompt: promptResults.aiPrompt
-}
+  personality: promptResults.personality,
+  aiPrompt: promptResults.aiPrompt,
+  outputType: promptResults.outputType,
+};
 
 const result = await execute(input);
-console.log(result);
+
+if (promptResults.outputType === "Console") {
+  console.log(result);
+} else {
+  exec.execSync(`open output/${result}`, { stdio: "inherit" });
+}
